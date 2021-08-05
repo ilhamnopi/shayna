e0c75096ee2a9d8e80564382e1bf40b74ee79786
ilhamnopihendri <ilhamnopihendri@gmail.com>
2021-08-05 18:17:18 +0700

    Selesai Templateing ke Vue

---
+++ src/components/PartnerLogoShayna.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="partner-logo">
+    <div class="container">
+      <carousel class="logo-carousel" :autoplay="true" :nav="false" :dots="false" :margin="30" :responsive="{ 0: { items: 3, nav: false }, 600: { items: 4, nav: false }, 800: { items: 5, nav: false } }">
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-1.png" alt="" />
+          </div>
+        </div>
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-2.png" alt="" />
+          </div>
+        </div>
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-3.png" alt="" />
+          </div>
+        </div>
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-4.png" alt="" />
+          </div>
+        </div>
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-5.png" alt="" />
+          </div>
+        </div>
+      </carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+import carousel from "vue-owl-carousel";
+export default {
+  name: "PartnerLogoShayna",
+  components: {
+    carousel,
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomanShayna />
+    <InstaShayna />
+    <PartnerLogoShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomanShayna from "@/components/WomanShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerLogoShayna from "@/components/PartnerLogoShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomanShayna,
+    InstaShayna,
+    PartnerLogoShayna,
+    FooterShayna,
+  },
+};
+</script>
